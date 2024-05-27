a3705822b7d328257f65f4268e81b32a439f4170
Oleg Zhdanenko <Olegzhdan1988@gmail.com>
2024-05-27 10:29:36 +0300

    fix

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-
 import contactsRouter from "./routes/contactsRouter.js";
-
+import "./DB/db.js"
 const app = express();
 
 app.use(morgan("tiny"));
@@ -13,14 +12,13 @@ app.use(express.json());
 app.use("/api/contacts", contactsRouter);
 
 app.use((_, res) => {
-  res.status(404).json({ message: "Route not found" });
+    res.status(404).json({ message: "Route not found" });
 });
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
-  res.status(status).json({ message });
+    const { status = 500, message = "Server error" } = err;
+    res.status(status).json({ message });
 });
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
+
+export default app;
--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,82 @@
-import contactsService from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
+import Contact from "../models/contact.js"
 
-export const getAllContacts = (req, res) => {};
 
-export const getOneContact = (req, res) => {};
+export const getAllContacts = async (req, res, next) => {
+    try {
+        const contact = await Contact.find()
+        res.json(contact)
+    } catch (error) {
+        next(error)
+    }
+};
 
-export const deleteContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+    try {
+        const id = req.params()
+        const contact = await Contact.findById(id)
+        res.status(200).json(contact)
+        if (!contact) {
+            throw HttpError(404);
+        }
+    } catch (error) {
+        next(HttpError(500));
+    }
+};
 
-export const createContact = (req, res) => {};
+export const deleteContact = async (req, res) => {
+    try {
+        const id = req.params()
+        const contact = await Contact.findByIdAndDelete(id)
+        res.status(200).json(contact)
+        if (contact===null) {
+            throw HttpError(404);
+        }
+    } catch (error) {
+        next(HttpError(500));
+    }
+};
 
-export const updateContact = (req, res) => {};
+export const createContact = async (req, res, next) => {
+    const contact = {
+        ...req.body,
+        favorite: false,
+    }
+    try {
+        const result = await Contact.create(contact);
+        res.status(201).json(result);
+    } catch (error) {
+        next(HttpError(500))
+    }
+    
+};
+export const updateContact = async (req, res) => {
+    const contact = {
+        ...req.body,
+        favorite: false,
+    };
+    try {
+        const { id } = req.params;
+        const result = await Contact.findByIdAndUpdate(id,contact)
+        res.status(200).json(result);
+        if (!result) {
+            throw HttpError(400);
+        }
+        res.json(result);
+    } catch (error) {
+        next(HttpError(500));  
+    }
+};
+export const updateStatusContact = async (req, res, next) => {
+    const { id } = req.params;
+    const body = req.body;
+    try {
+        const contact = await Contact.findOneAndUpdate({ owner: req.user._id, _id: id }, body)
+        res.status(200).json(contact)
+        if (!contact) {
+            throw HttpError(404);
+        }
+    } catch (error) {
+        next(HttpError(500));
+    }
+};
